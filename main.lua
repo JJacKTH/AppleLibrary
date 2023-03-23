678f6a24030051a556e183366b28e817d4f005c8
JJacKTH <109510740+JJacKTH@users.noreply.github.com>
2023-03-23 16:46:29 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -76,12 +76,22 @@ function lib:init(ti, dosplash, visiblekey, deleteprevious)
         sicon.ScaleType = Enum.ScaleType.Fit
         sicon.TileSize = UDim2.new(1, 0, 20, 0)
 
-        local ug = Instance.new("UIGradient")
-        ug.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 255)), ColorSequenceKeypoint.new(0.01, Color3.fromRGB(61, 61, 61)), ColorSequenceKeypoint.new(0.47, Color3.fromRGB(41, 41, 41)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(0, 0, 0))}
-        ug.Rotation = 90
-        ug.Parent = sicon
-
-       
+        local sshadow = Instance.new("ImageLabel")
+        sshadow.Name = "sshadow"
+        sshadow.Parent = splash
+        sshadow.AnchorPoint = Vector2.new(0.5, 0.5)
+        sshadow.BackgroundTransparency = 1
+        sshadow.Position = UDim2.new(0.5, 0, 0.5, 0)
+        sshadow.Size = UDim2.new(1.20000005, 0, 1.20000005, 0)
+        sshadow.ZIndex = 39
+        sshadow.Image = "rbxassetid://313486536"
+        sshadow.ImageColor3 = Color3.fromRGB(0, 0, 0)
+        sshadow.ImageTransparency = 0.400
+        sshadow.TileSize = UDim2.new(0, 1, 0, 1)
+        splash:TweenPosition(UDim2.new(0.5, 0, 0.5, 0), "InOut", "Quart", 1)
+        wait(2)
+        splash:TweenPosition(UDim2.new(0.5, 0, 2, 0), "InOut", "Quart", 1)
+        game:GetService("Debris"):AddItem(splash, 1)
     end
         
 
